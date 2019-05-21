51f6a1b327ffa830248d772b9ee9c400f0a4c263
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 11:30:30 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            
+            Console.Write("\nInput the First Number : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            b = int.Parse(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + a);
+            Console.Write("\nSecond Number : " + b);
+            Console.Read();
+        }
+    }
+}
